ea67b7882ea7607283809d736bb31764bd1535f7
zulfilee <zulfilee@gmail.com>
2020-07-19 13:51:40 +0530

    feat: Test something

--- webserver/main.go
+++ webserver/main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"github.com/airbrake/gobrake"
 	"net/http"
 )
 
-var airbrake = gobrake.NewNotifierWithOptions(&gobrake.NotifierOptions{
-	ProjectId:   252403,
-	ProjectKey:  "302de0f68f1987b32b080b1ccf647ea4",
-	Environment: "production",
-})
+//var airbrake = gobrake.NewNotifierWithOptions(&gobrake.NotifierOptions{
+//	ProjectId:   252405,
+//	ProjectKey:  "c032c12b1096cb8fec0dabf7c186ad9a",
+//	Environment: "dev",
+//})
 
-func init() {
-    airbrake.AddFilter(func(notice *gobrake.Notice) *gobrake.Notice {
-        notice.Params["user"] = map[string]string{
-            "id": "1",
-            "username": "johnsmith",
-            "name": "John Smith",
-        }
-        return notice
-    })
-}
+//func init() {
+//    airbrake.AddFilter(func(notice *gobrake.Notice) *gobrake.Notice {
+//       notice.Params["user"] = map[string]string{
+//          "id": "1",
+//        "username": "johnsmith",
+//      "name": "John Smith",
+// }
+//return notice
+// })
+//}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello from Spectro!")
 }
 
 func main() {
-	defer airbrake.Close()
-	defer airbrake.NotifyOnPanic()
+	//	defer airbrake.Close()
+	//	defer airbrake.NotifyOnPanic()
 
-	airbrake.Notify(errors.New("operation failed"), nil)
+	//	airbrake.Notify(errors.New("operation failed"), nil)
 	http.HandleFunc("/hello-backend", handler)
 	fmt.Println("Server running...")
 
